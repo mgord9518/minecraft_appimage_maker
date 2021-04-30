0c36a408ba25bc1a13f5d7b4acae2ac888522708
mgord9518 <72643694+mgord9518@users.noreply.github.com>
2021-05-01 01:19:57 +0900

    Change a lot
    Remove code to download Java since the actual launcher now does it, change the way it looks while running, change the appimagetool download link to point to continuous build, and more stuff

--- minecraft-appimage-maker.bash
+++ minecraft-appimage-maker.bash
@@ -1,113 +1,87 @@
 #!/bin/bash
 
-# VERSION 0.3.1
-# Changes:
-#  * Added the 'include' directory to be stripped out of the embedded OpenJRE
-#  * Moved the code to get the current JRE download to under the code of changing directories into the TEMP_DIR. This is so out.log doesn't generate in the starting directory
+# Copyright (C) 2020-2021 Mathew R Gordon
+#    <https://github.com/mgord9518>
 
-# CONSTANTS
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# VERSION 0.5
+
+# Variables
 TMP_DIR="/tmp/.minecraft_appimage_maker_${RANDOM}"
 START_DIR=${PWD}
 MINECRAFT_LINK='https://launcher.mojang.com/download/Minecraft.tar.gz'
 MINECRAFT_ICON='https://launcher.mojang.com/download/minecraft-launcher.svg'
-APPIMAGETOOL_LINK='https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage'
-OUTPUT_APPIMAGE='Minecraft.AppImage'
+APPIMAGETOOL_LINK='https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage'
+
+# Define what should be in the desktop entry
+DESKTOPFILE='[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Minecraft
+Comment=Minecraft Launcher
+Exec=minecraft-launcher
+Icon=minecraft-launcher
+Terminal=false
+Categories=Game;Application;
+X-AppImage-Name=Minecraft'
+
+# Define what should be in the AppRun script
+APPRUN='#!/bin/sh
+SELF=$(readlink -f "$0")
+HERE=${SELF%/*}
+export PATH="$HERE/:PATH"
+exec minecraft-launcher $@'
+
+# Set color vars to their ANSI values
+R="\e[0;91m"
+W="\e[0;97m"
 
 # Create and move to working directory
 mkdir ${TMP_DIR} && cd ${TMP_DIR}
 mkdir AppDir
-
-# Get the link source using standard GNU commands, this needs to be done because the link doesn't stay the same with new versions
-JRE_LINK=$(wget 'https://jdk.java.net/java-se-ri/8-MR3' -O - -o out.log | grep 'linux-x64' | cut -d '"' -f 2 | head -n 1) # This could possibly break in the future if they change the layout of the website
-if [ ! ${?} = 0 ]; then
-	echo -e "${R} > ERROR:${W} Failed to obtain the current release of OpenJRE 8 (this could be caused by a change in the website layout). Check out.log"
-	mv out.log ${START_DIR}/out.log
-	exit 1
-fi
-
-# The desktop entry to be packed into the final AppImage
-DESKTOPFILE='[Desktop Entry]
-Type=Application
-Version=1.0
-Name=Minecraft Launcher
-Comment=Official Minecraft Launcher (unofficial AppImage build)
-Exec=AppRun
-Icon=minecraft-launcher
-Terminal=false
-Categories=Game;Application;'
-
-# AppRun script (needed to start the Minecraft Launcher)
-APPRUN='#!/bin/bash
-export PATH="${APPDIR}/OpenJRE/jre/bin/:${PATH}"
-${APPDIR}/minecraft-launcher/minecraft-launcher'
-
-# Colors
-G="\e[0;92m"
-R="\e[0;91m"
-W="\e[0;97m"
-# END CONSTANTS
-
-echo -e "${G}STARTING MINECRAFT APPIMAGE BUILDER"
-echo -e " > ${W}Working directory: ${PWD}"
+echo -e "Working directory: ${PWD}"
 
 # Download the latest Minecraft tarball
-echo -e "${G} > ${W}Downloading latest Minecraft archive..."
+echo -e "Downloading Minecraft..."
 wget ${MINECRAFT_LINK} -O Minecraft.tar.gz -o out.log
 if [ ! ${?} = 0 ]; then
-	echo -e "${R} > ERROR:${W} Failed to download Minecraft.tar.gz (make sure you're connected to the internet). Check out.log"
-	mv out.log ${START_DIR}/out.log
+	echo -e "${R}ERROR:${W} Failed to download Minecraft.tar.gz (make sure you're connected to the internet). Check out.log"
 	exit 1
 fi
 
-# Download the latest JRE tarball
-echo -e "${G} > ${W}Downloading latest OpenJRE archive..."
-wget ${JRE_LINK} -O OpenJRE.tar.gz -o out.log
-if [ ! ${?} = 0 ]; then
-	echo -e "${R} > ERROR:${W} Failed to download OpenJRE.tar.gz. Check out.log"
-	mv out.log ${START_DIR}/out.log
-	exit 1
-fi
-
-# Download the icon
-echo -e "${G} > ${W}Downloading Minecraft icon..."
+# Download the Minecraft icon
 wget ${MINECRAFT_ICON} -O AppDir/minecraft-launcher.svg -o out.log
 if [ ! ${?} = 0 ]; then
-	echo -e "${R} > ERROR:${W} Failed to download minecraft-launcher.svg (make sure you're connected to the internet). Check out.log"
-	mv out.log ${START_DIR}/out.log
-	exit 1
+	echo -e "${R}ERROR:${W} Failed to download minecraft-launcher.svg (make sure you're connected to the internet). Check out.log"
+	exit 2
 fi
 
-# Extract the tarballs
-echo -e "${G} > ${W}Extracting files..."
-tar -xzf Minecraft.tar.gz -C AppDir/
-tar -xzf OpenJRE.tar.gz -C AppDir/
-
-# Rename the extracted JRE to something more accessable
-mv AppDir/*java* AppDir/OpenJRE
-
-# Remove the source code, demos, man pages, JDK and header files from OpenJRE because we don't need them in the AppImage and would just take up space
-echo -e "${G} > ${W}Removing unneccessary files from the JRE..."
-rm -r AppDir/OpenJRE/src.zip \
-      AppDir/OpenJRE/demo    \
-      AppDir/OpenJRE/sample  \
-      AppDir/OpenJRE/man     \
-      AppDir/OpenJRE/bin     \
-      AppDir/OpenJRE/lib     \
-	  AppDir/OpenJRE/include
+# Extract the tarball
+echo -e "Extracting..."
+tar -xzf Minecraft.tar.gz --strip-components=1 -C AppDir/
 
 # Write our ${DESKTOPFILE} variable to the desktop entry
 echo "${DESKTOPFILE}" > AppDir/minecraft-launcher.desktop
 echo "${APPRUN}" > AppDir/AppRun
 
 # Make both the desktop file and the AppRun script executable
-chmod +x AppDir/minecraft-launcher.desktop \
-         AppDir/AppRun
+chmod +x AppDir/minecraft-launcher.desktop
+chmod +x AppDir/AppRun
 
-# Link up the icon to '.DirIcon', because it is neccessary in order to display the icon on the resulting AppImage (assuming that a thumbnailer daemon is running)
+# Link up the icon we have to .DirIcon (needed file to display the icon on the finished AppImage)
 ln -s minecraft-launcher.svg AppDir/.DirIcon
 
-# Check if user has AppImageTool in path (under the likely names of "appimagetool", "appimagetool.AppImage" and appimagetool-x86_64.AppImage) if not, download it
-echo -e "${G} > ${W}Checking if AppImageTool is installed..."
+echo -e "Checking if AppImageTool is installed..."
+# Check if user has AppImageTool in path (under the possible names of "appimagetool", "appimagetool.AppImage" and appimagetool-x86_64.AppImage) if not, download it
 if hash appimagetool &> /dev/null; then
 	APPIMAGETOOL='appimagetool'
 elif hash appimagetool.AppImage &> /dev/null; then
@@ -115,41 +89,34 @@ elif hash appimagetool.AppImage &> /dev/null; then
 elif hash appimagetool-x86_64.AppImage &> /dev/null; then
 	APPIMAGETOOL='appimagetool-x86_64.AppImage'
 else
-	echo -e "${G} > ${W}Nope! (couldn't find it) Downloading it now..."
+	echo -e "Nope! (couldn't find it) Downloading it now..."
 	wget ${APPIMAGETOOL_LINK} -O appimagetool.AppImage -o out.log
 	if [ ! ${?} = 0 ]; then
-		echo -e "${R} > ERROR:${W} Failed to download AppImageTool.AppImage. Check out.log"
-		mv out.log ${START_DIR}/out.log
-		exit 1
+		echo -e "${R}ERROR:${W} Failed to download AppImageTool.AppImage (make sure you're connected to the internet). Check out.log"
+		exit 3
 	fi
 	chmod +x appimagetool.AppImage
 	APPIMAGETOOL='./appimagetool.AppImage'
 fi
 
-# Use whichever AppImageTool we found to create the AppImage
-echo -e "${G} > ${W}Building ${OUTPUT_APPIMAGE}..."
-${APPIMAGETOOL} AppDir/ &> out.log
+# Use the found AppImageTool to build our AppImage
+echo -e "Building Minecraft.AppImage..."
+ARCH=x86_64 ${APPIMAGETOOL} AppDir/ &> out.log
+
 if [ ! ${?} = 0 ]; then
-	echo -e "${R} > ERROR:${W} Failed to build ${OUTPUT_APPIMAGE}. Check out.log"
-	mv out.log ${START_DIR}/out.log
-	exit 1
+	echo -e "${R}ERROR:${W} Failed to build Minecraft.AppImage. Check out.log"
+	exit 4
 fi
 
-# Move the AppImage into the current directory
-echo -e "${G} > ${W}Moving ${OUTPUT_APPIMAGE} to current directory..."
-if [ -f ${START_DIR}/${OUTPUT_APPIMAGE} ]; then
-	echo -e "${G} > ${W}File exists; overwriting..."
-	rm ${START_DIR}/${OUTPUT_APPIMAGE}
+# Take the newly created AppImage and move it into the starting directory
+if [ -f ${START_DIR}/Minecraft-x86_64.AppImage ]; then
+	echo -e "AppImage already exists; overwriting..."
+	rm ${START_DIR}/Minecraft-x86_64.AppImage
 fi
-mv 'Minecraft_Launcher-x86_64.AppImage' ${START_DIR}/${OUTPUT_APPIMAGE} &> out.log
-if [ ! ${?} = 0 ]; then
-	echo -e "${R} > ERROR:${W} Failed to move ${OUTPUT_APPIMAGE} into the current directory. Check out.log"
-	mv out.log ${START_DIR}/out.log
-	exit 1
-fi
+mv 'Minecraft-x86_64.AppImage' ${START_DIR}
 
-# Remove all temporary files (including ones from previous runs that may have been cancelled)
-echo -e "${G} > ${W}Cleaning up..."
+# Remove all temporary files
+echo -e "Cleaning up..."
 rm -rf /tmp/.minecraft_appimage_maker_*
 
-echo -e "${G}DONE! Enjoy playing!${W}"
+echo -e "DONE! Enjoy playing!${W}"
